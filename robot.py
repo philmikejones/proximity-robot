d74ec2031e9475e1dec6abad0d12720288f04e76
Phil Mike Jones <philmikejones@gmail.com>
2019-10-29 11:23:23 +0000

    add threshold as variable

--- robot.py
+++ robot.py
@@ -6,6 +6,8 @@ import busio
 import adafruit_vcnl4010
 import time
 
+threshold = 2500
+
 # # uncomment one or both of these lines to correct if any of the motors travel backwards!
 # explorerhat.motor.one.invert()
 # explorerhat.motor.two.invert()
@@ -14,9 +16,9 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_vcnl4010.VCNL4010(i2c)
 
 while True:
-	if sensor.proximity < 2500:
+	if sensor.proximity < threshold:
 		explorerhat.motor.forwards()
-	elif sensor.proximity > 2501:
+	elif sensor.proximity >= threshold:
 		explorerhat.motor.stop()
 		explorerhat.motor.backwards()
 		time.sleep(1.0)
